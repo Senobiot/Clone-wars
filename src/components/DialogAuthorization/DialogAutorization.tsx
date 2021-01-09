4d63c7cbd7ac4771efe9822d12b14584798b39b1
cup0ra <cup0ra@users.noreply.github.com>
2021-01-09 21:33:40 +0300

    feat: added authentication firebase

--- src/components/DialogAuthorization/DialogAutorization.tsx
+++ src/components/DialogAuthorization/DialogAutorization.tsx
@@ -2,33 +2,32 @@ import { Dialog, IconButton, Slide, SlideProps } from '@material-ui/core';
 import React, { useEffect } from 'react';
 import CloseIcon from '@material-ui/icons/Close';
 import Authorization from '../Authorization/Authorization';
-import './DialogAutorization.scss';
+import style from './DialogAutorization.module.scss';
 
 const Transition = React.forwardRef(function Transition(props: SlideProps, ref): JSX.Element {
-    return <Slide direction="up" ref={ref} {...props} />;
+  return <Slide direction="up" ref={ref} {...props} />;
 });
 interface Props {
-    openDialog: boolean;
-    handleClickOpenDialog: () => void;
+  openDialog: boolean;
+  handleClickOpenDialog: () => void;
 }
 
 export function DialogAutorization({ openDialog, handleClickOpenDialog }: Props): JSX.Element {
-    const [open, setOpen] = React.useState<boolean>(false);
-    useEffect(() => setOpen(openDialog), [openDialog]);
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const [open, setOpen] = React.useState<boolean>(false);
+  useEffect(() => {
+    setOpen(openDialog);
+  }, [openDialog]);
 
-    const handleCloseDiolog = () => {
-        setOpen(false);
-        handleClickOpenDialog();
-    };
-    return (
-        <Dialog fullScreen open={open} onClose={handleCloseDiolog} TransitionComponent={Transition}>
-            <IconButton className="button" color="secondary" aria-label="add an alarm" onClick={handleCloseDiolog}>
-                <CloseIcon />
-            </IconButton>
-            <Authorization />
-        </Dialog>
-    );
+  const handleCloseDiolog = () => {
+    setOpen(false);
+    handleClickOpenDialog();
+  };
+  return (
+    <Dialog fullScreen open={open} onClose={handleCloseDiolog} TransitionComponent={Transition}>
+      <IconButton className={style.button} color="secondary" aria-label="add an alarm" onClick={handleCloseDiolog}>
+        <CloseIcon />
+      </IconButton>
+      <Authorization handleClickOpenDialog={handleClickOpenDialog} />
+    </Dialog>
+  );
 }
--- src/services/updateFirebase.ts
+++ src/services/updateFirebase.ts
@@ -1,64 +1,111 @@
-import { db } from '../../firebase';
+import firebase from 'firebase';
+import { db, provider } from '../../firebase';
 
 export const getCollection = async (collection: string): Promise<any[]> => {
-    let data = [];
-    await db
-        .collection(collection)
-        .get()
-        .then((snap) => {
-            data = snap.docs.map((doc) => doc.data());
-        });
-    return data;
+  let data = [];
+  await db
+    .collection(collection)
+    .get()
+    .then((snap) => {
+      data = snap.docs.map((doc) => doc.data());
+    });
+  return data;
 };
 
 export const getDocument = async (collection: string, doc: string): Promise<any> => {
-    let object: any = {};
-    await db
-        .collection(collection)
-        .doc(doc)
-        .get()
-        .then((doc) => {
-            if (doc.exists) {
-                object = { ...object, ...doc.data() };
-            } else {
-                console.log('No such document!');
-            }
-        });
-    return object;
+  let object: any = {};
+  await db
+    .collection(collection)
+    .doc(doc)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        object = { ...object, ...doc.data() };
+      } else {
+        console.log('No such document!');
+      }
+    });
+  return object;
 };
 
 export const setDocument = (collection: string, doc: string, object: any): void => {
-    db.collection(collection)
-        .doc(doc)
-        .set(object)
-        .then(function () {
-            console.log('Document successfully written!');
-        })
-        .catch(function (error) {
-            console.error('Error writing document: ', error);
-        });
+  db.collection(collection)
+    .doc(doc)
+    .set(object)
+    .then(function () {
+      console.log('Document successfully written!');
+    })
+    .catch(function (error) {
+      console.error('Error writing document: ', error);
+    });
 };
 
 export const updateObjectField = (collection: string, doc: string, objectField: any): void => {
-    db.collection(collection)
-        .doc(doc)
-        .update(objectField)
-        .then(function () {
-            console.log('Document successfully written!');
-        })
-        .catch(function (error) {
-            console.error('Error writing document: ', error);
-        });
+  db.collection(collection)
+    .doc(doc)
+    .update(objectField)
+    .then(function () {
+      console.log('Document successfully written!');
+    })
+    .catch(function (error) {
+      console.error('Error writing document: ', error);
+    });
 };
 
 export const deleteDocument = (collection: string, doc: string): void => {
-    db.collection(collection)
-        .doc(doc)
-        .delete()
-        .then(function () {
-            console.log('Document successfully written!');
-        })
-        .catch(function (error) {
-            console.error('Error removing document: ', error);
-        });
+  db.collection(collection)
+    .doc(doc)
+    .delete()
+    .then(function () {
+      console.log('Document successfully written!');
+    })
+    .catch(function (error) {
+      console.error('Error removing document: ', error);
+    });
+};
+
+export const signUp = (email: string, password: string, toggleDialog: () => void, setstateAlert: any): void => {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+      toggleDialog();
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      setstateAlert({ open: true, message: errorMessage });
+    });
+};
+export const signIn = (email: string, password: string, toggleDialog: () => void, setstateAlert: any): void => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      toggleDialog();
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      setstateAlert({ open: true, message: errorMessage });
+    });
+};
+
+export const singUpGoogle = (toggleDialog: () => void): void => {
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      const user: firebase.User = result.user;
+      console.log(user.providerData);
+      toggleDialog();
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      const credential = error.credential;
+      // ...
+    });
 };
