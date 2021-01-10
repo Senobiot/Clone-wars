b7fff0765cefa934a70fc3bedda90d76d665eea1
Aliaksandr Kulik <inter_net@tut.by>
2021-01-10 17:06:46 +0300

    fix: resolve conflict

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'
 import {Footer} from '../Footer';
 import {Header} from '../Header';
 import MedicsList from '../MedicsList/MedicsList'
-import {MedicPage} from '../MedicPage/MedicPage'
 import '../../global/variables.scss';
 import '../../global/reset.scss';
 import { Switch } from '@material-ui/core';
@@ -12,10 +10,7 @@ export default function App() {
   return (
     <>
       <Header></Header>
-      <BrowserRouter>
-          <Route path={'/'} exact component={MedicsList}/>
-          <Route path={'/medicPage'} component={MedicPage}/>   
-      </BrowserRouter>
+        <MedicsList></MedicsList>
       <Footer></Footer>
     </>
   );
