4b8f9b1fd653c029ff4334d9f811f021de9f67b6
Aliaksandr Kulik <inter_net@tut.by>
2021-01-12 04:19:27 +0300

    feat: add map with markers

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@ import {Footer} from '../Footer';
 import {Header} from '../Header';
 import MedicsList from '../MedicsList/MedicsList';
 import MedicPage from '../MedicPage/MedicPage';
+import MedcentersList from '../MedcentersList/MedcentersList';
 import '../../global/variables.scss';
 import '../../global/reset.scss';
 import { BrowserRouter, Route} from "react-router-dom";
@@ -13,6 +14,7 @@ export default function App() {
      <BrowserRouter>
       <Header></Header>
           <Route path={'/'} exact component={null}></Route>
+          <Route path={'/MedcentersList/'} component={MedcentersList}></Route>
           <Route path={'/MedicsList/'} component={MedicsList}></Route>
           <Route path={'/MedicPage/:id'} component={MedicPage}></Route>
         <Footer></Footer> 
---
+++ src/components/MedcentersList/MedcentersList.tsx
@@ -0,0 +1,61 @@
+import React,  { useState, useEffect } from 'react';
+import styles from './Medcenters.module.scss';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { med_centers } from '../../data/medcentersList';
+import Button from '@material-ui/core/Button';
+// import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//     root: {
+//       '& > *': {
+//         margin: theme.spacing(1),
+//       },
+//     },
+//   }),
+// );
+
+function ContainedButtons() {
+//   const classes = useStyles();
+
+  return (
+    <div className={styles.button}>
+      <Button variant="contained">
+        Подробнее
+      </Button>
+    </div>
+  );
+}
+
+const MedcentersList = ({}) => {
+
+    return (
+        <div className={styles.MedcentersListWrapper}>
+            <div className={styles.MedcentersListBlock}></div>
+            <MapContainer center={[53.9, 27.56667]} zoom={12} scrollWheelZoom={true} className={styles.leafletContainer}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {
+                    med_centers.map(e => {
+                        const coords = e.coord.split(',');
+                    return (
+                    <Marker position={[parseFloat(coords[0]) , parseFloat(coords[1])]}>
+                        <Popup className={styles.leafletPopup}>
+                            <img src={'../' + e.logo}></img>
+                            <div>{e.adress}</div>
+                            <div>{e.fullname}</div>
+                            <div>{ContainedButtons()}</div>    
+                        </Popup>
+                    </Marker> 
+                    )
+                    })
+                }
+               
+            </MapContainer>
+        </div>
+    );
+};
+
+export default MedcentersList;
