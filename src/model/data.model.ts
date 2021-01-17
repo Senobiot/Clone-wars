e7810e6df698adc5852e74bf87c6f005e61e4974
cup0ra <cup0ra@users.noreply.github.com>
2021-01-17 16:24:38 +0300

    feat: added interfase for data

---
+++ src/model/data.model.ts
@@ -0,0 +1,51 @@
+export interface IMedicsList {
+  speciality: string;
+  medcenter: string;
+  category: string;
+  experience: string;
+  graduation: string;
+  id: string;
+  img: string;
+  name: string;
+  phone: string;
+}
+
+export interface IUser extends IMedicsList {
+  email: string;
+  password: string;
+  gender: string;
+  birthday: string;
+  role: Role;
+}
+
+export enum Role {
+  Doctor = 'doctor',
+  Patient = 'patient',
+}
+
+export interface IServicesCategory {
+  id: string;
+  category_name: string;
+  medic: string | null;
+  services: IServices;
+}
+
+interface IServices {
+  [name: string]: string;
+}
+
+export interface IMmedCenters {
+  name: string;
+  fullname: string;
+  adress: string;
+  img: string;
+  logo: string;
+  coord: string;
+  phones: string[];
+  schedule: string;
+  services: { [name: string]: IPrice[] };
+}
+
+interface IPrice {
+  [name: string]: number;
+}
