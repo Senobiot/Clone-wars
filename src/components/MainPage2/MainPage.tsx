d57c23c4d5b9bcb53334884fb1c61277e4d64336
Aliaksandr Kulik <inter_net@tut.by>
2021-01-21 01:48:40 +0300

    feat: add parallax pics

--- src/components/MainPage2/MainPage.tsx
+++ src/components/MainPage2/MainPage.tsx
@@ -1,40 +1,27 @@
 import React, {useRef} from 'react';
 import styles from './MainPage.module.scss';
 import { Link } from 'react-router-dom';
+import { useSpring, animated } from 'react-spring'
+
+const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
+const trans1 = (x, y) => `translate3d(${x / 100}px,${y / 10}px,0)`
+const trans2 = (x, y) => `rotate(${x / 8 + 35}deg)`
+const trans3 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`
+const trans4 = (x, y) => `translate3d(${x / 3.5}px,${y / 10}px,0)`
+const trans5 = (x, y) => `translate3d(${x / 10}px,${y / 15}px,0)`
+const trans6 = (x, y) => `translate3d(${x / 50}px,${y / 25}px,0)`
 
 export function MainPage() {
-      const activeSlideRef = useRef(null);
-      let elem = activeSlideRef as unknown as HTMLElement;
-  function ats () {
-
-    // Add event listener
-    document.addEventListener("mousemove", parallax);
-    
-    console.log(elem)
-
-    // Magic happens here
-    function parallax(e) {
-        let _w = window.innerWidth/2;
-        let _h = window.innerHeight/2;
-        let _mouseX = e.clientX;
-        let _mouseY = e.clientY;
-        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
-        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
-        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
-        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
-      console.log(elem.style.backgroundPosition = '200')
-        // elem.style.backgroundPosition = x;
-    }
-
-};
-  setTimeout(()=> {
-    elem = activeSlideRef as unknown as HTMLElement;
-    ats ();
-  }, 1000);
-
+  const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
     return (
       <>
-        <div className={styles.mainPage} ref={activeSlideRef}>
+        <div className={styles.mainPage} onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+        <animated.div className={styles.card1} style={{ transform: props.xy.interpolate(trans1) }} />
+        <animated.div className={styles.card2} style={{ transform: props.xy.interpolate(trans2) }} />
+        <animated.div className={styles.card3} style={{ transform: props.xy.interpolate(trans3) }} />
+        <animated.div className={styles.card4} style={{ transform: props.xy.interpolate(trans4) }} />
+        <animated.div className={styles.card5} style={{ transform: props.xy.interpolate(trans5) }} />
+        <animated.div className={styles.card6} style={{ transform: props.xy.interpolate(trans6) }} />
             <div className={styles.mainPageBtnWrapper}>
             {['Найти свой медцентр', ' Найти своего врача', ' Посмотреть услуги', 'Зайти в свой кабинет'].map((text, i) => {
                 return (
@@ -48,8 +35,7 @@ export function MainPage() {
                         {text}
                         </div>  
                        </Link>
-                )
-            
+                )          
             })}
         </div>
         </div>
